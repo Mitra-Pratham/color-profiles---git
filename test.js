022d93d1076e88f71f493bc3362d01798d9c5851
Pratham <pratham.sethi@mitratech.com>
2024-05-27 18:39:24 +0530

    Multiple updates - dark less file, table of contents and links creator

--- test.js
+++ test.js
@@ -1,5 +1,35 @@
-jQuery('#embeddedObject td:nth-child(5)').each(function(){
-    let tempName = jQuery(this).text();
-    let tempHTML= `<a href='https://google.com' target="_blank">${tempName}</a>`
-    jQuery(this).html(tempHTML);
+// links creator
+let auditIndex;
+
+jQuery('#embeddedObject th').each(function () {
+    if (jQuery(this).text().includes('Audit')) {
+        auditIndex = Number(jQuery(this).index()) + 1;
+    }
+});
+
+
+jQuery(`#embeddedObject td:nth-child(${auditIndex})`).each(function () {
+    let tempURL = jQuery(this).text();
+    if (tempURL.includes('https://')) {
+        let tempName = `Go to Audit Trail`;
+        let tempHREF = `href="${tempURL}"`;
+        let tempHTML = `<a ${tempHREF} target="_blank">${tempName}</a>`
+        jQuery(this).html(tempHTML);
+    }
+});
+
+
+//table of contents
+
+jQuery('.bg_blockhdr h3').each(function () {
+    let header = jQuery(this).text();
+    let headerVal=header.replace(/ /g,'-')
+    jQuery(this).attr('id', `${headerVal}`);
+    let tocItem = `<a val=${headerVal} class="toc-item" href="#">${header}</a>`;
+    jQuery('#TOCDiv').append(tocItem);
+});
+
+jQuery('#TOCDiv').on('click', '.toc-item', function () {
+    let scrollItem = jQuery(this).attr('val');
+    jQuery(`#${scrollItem}`)[0].scrollIntoView();
 });
